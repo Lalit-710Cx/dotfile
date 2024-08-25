ae2caf4867c3ae53526cb7ea9dc0baa5cbcb5822
Lalit-710Cx <code.bikram.007@gmail.com>
2024-08-25 23:20:39 +0530

    adding all

---
+++ backup_scripts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# author: Lalit-710Cx
+# WrittenTime 25/08/2024 21:59:20
+# Github Repo: 'https://github.com/Lalit-710Cx'
+
+cp -r /home/bikram007/.zshrc /home/bikram007/dotfile/shell/
+cp -r /home/bikram007/.p10k.zsh /home/bikram007/dotfile/shell/
+cp -r /home/bikram007/.bashrc /home/bikram007/dotfile/shell/
+
+cp -r /home/bikram007/.config/hypr /home/bikram007/dotfile/
+
+cp -r /home/bikram007/.config/nvim /home/bikram007/dotfile/
+cp -r /home/bikram007/.config/yazi /home/bikram007/dotfile/
+
+cp -r /home/bikram007/.config/kitty /home/bikram007/dotfile/
+cp -r /home/bikram007/.config/spicetify /home/bikram007/dotfile/
